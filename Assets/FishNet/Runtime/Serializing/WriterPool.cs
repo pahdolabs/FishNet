926df89465003470218a23e73e5f06bdc893b398
Zack Darden <zack@pahdolabs.com>
2025-09-03 17:30:26 -0400

    Update WriterPool.cs - remove extra logging

--- Assets/FishNet/Runtime/Serializing/WriterPool.cs
+++ Assets/FishNet/Runtime/Serializing/WriterPool.cs
@@ -112,7 +112,6 @@ namespace FishNet.Serializing
             //There is already one pooled.
             if (_lengthPool.TryGetValue(index, out stack) && stack.Count > 0)
             {
-                Debug.Log($"Pooled - Checked WriterPool at index: {index} for length: {length}, count: {stack.Count}, _lengthPool.Count: {_lengthPool.Count}, _lengthPool.Keys: [{string.Join(',',_lengthPool.Keys)}]");
                 PooledWriter result = stack.Pop();
                 result.Reset(networkManager);
                 return result;
@@ -120,7 +119,6 @@ namespace FishNet.Serializing
             //Not pooled yet.
             else
             {
-                Debug.Log($"NotPooled - Checked WriterPool at index: {index} for length: {length}, count: {stack?.Count ?? -1}, _lengthPool.Count: {_lengthPool.Count}, _lengthPool.Keys: [{string.Join(',',_lengthPool.Keys)}]");
                 //Get any ol' writer.
                 PooledWriter writer = Retrieve(networkManager);
                 /* Ensure length to fill it's bracket.
@@ -238,4 +236,5 @@ namespace FishNet.Serializing
 
     }
 }
+
 
